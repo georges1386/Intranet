f182108e4d8cc44f74e8032e6b6f680ae3101e29
Georges-Noé <georges.ahombo@gmail.com>
2025-01-07 08:59:36 +0000

    Jour 0: Mise en place du template & installation des dépendances
    Création du modèle à suivre
            - Le dossier api pour les api à mettre en place
            - Le dossier models pour l'ORM
            - Le dossier integrations pour les fichiers à intégrer dans le frontend
            - Le dossier views pour la mise en place du frontend
            - Le dossier security pour la mise en place des fonctions de sécurité
            - Installation des dépendances requises (express, ejs, socket.io, multe>
            - Création du fichier index.js, point d'entrée principal
            - Ajout	du script de déploiement au fichier package.json

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Intranet
 Un projet dévéloppé spécialement pour notre classe afin de faciliter les échanges d'informations
+
+---------
+
+Jour 0:
+	- Mise en place du template
---
+++ index.js
@@ -0,0 +1,13 @@
+// Structure du fichier
+
+// Les imports nécéssaires
+
+
+
+// importer les fichiers de l'API mis en place
+
+
+// Gestion des logs
+
+
+// Lancement du serveur
